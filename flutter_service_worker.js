09b34533fdde026e096e0498dec90a561ae9376a
Nguyen Quoc Huy <huynq_1@digi-texx.vn>
2023-01-08 21:46:05 +0700

    sss

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "assets/assets/images/logo.png": "acf38ac3e73743b12e67079b978e7452",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "cbc374dc8ee10dff64d76fb79f688d32",
+"assets/NOTICES": "d281772e0c211e39703ccbeaa6628eab",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "af4331b809f5a92ba40a697e9b27fa33",
-"/": "af4331b809f5a92ba40a697e9b27fa33",
-"main.dart.js": "ffb7b120f7a71b59d8f792e37c2286f6",
+"index.html": "7715eef1a7c441abc98058bd147d2b6a",
+"/": "7715eef1a7c441abc98058bd147d2b6a",
+"main.dart.js": "a35c4b1562f499f10b20434cf3eb0f40",
 "manifest.json": "59890d9c0299b7178a15afac37d3a27b",
 "version.json": "f214e5d7709cb7f3bfe4da53fbd7477c"
 };
